c4b309c4fa00d8ae64f2af191bc1756edc681e3b
S1S13AF7 <S1S13AF7@MISC.PP.UA>
2024-07-29 04:39:44 +0300

    Add files via upload

---
+++ misc_beta_bot.py
@@ -0,0 +1,58 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    #print(message)
+    await message.answer(emoji="🖖")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    #print(message)
+    await message.answer(emoji="🤷")
+
+@dp.message_handler(commands=['ping'])
+async def process_ping_command(message: types.Message):
+    #print(message)
+    await message.reply("PONG!")
+
+@dp.message_handler(commands=['code'])
+async def cmd_code(message: types.Message):
+    #print(message)
+    await message.reply("https://github.com/S1S13AF7/misc_beta_bot")
+
+@dp.message_handler(commands=['кубик'])
+async def cmd_dice(message: types.Message):
+    #print(message)
+    await message.answer_dice(emoji="🎲")
+
+@dp.message_handler(commands=['coffee','кава','кофе','кофи','кофі'])
+async def cmd_coffee(message: types.Message):
+    #print(message)
+    await message.answer("☕️")
+
+@dp.message_handler(commands=['tea','чай','чяй'])
+async def cmd_tea(message: types.Message):
+    #print(message)
+    await message.answer("🍵")
+
+@dp.message_handler(commands=['chats','чати','чаты','чаті'])
+async def cmd_chats(message: types.Message):
+    #print(message)
+    await message.answer('''
+•   ☕️ @misc_chat
+•   🦠 @misc_games
+•   🗃 @misc_files_v2
+''')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
